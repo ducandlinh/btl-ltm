5e31a919cf7c3deb256117bd849ae5cfdc3f313a
Luu Minh Duc <luuducfb123@gmail.com>
2026-09-22 08:47:38 +0700

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/btl-ltm/',
+
   server: {
     port: 5173,
     host: true,
   },
+
   build: {
     target: 'esnext',
   },
